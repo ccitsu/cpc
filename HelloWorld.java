879fe88f2d278fe9b66768b864aac6962ddd3841
nayyarahmed <ccitlms@gmail.com>
2022-06-28 20:07:06 +0300

    Laptop Commit!

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld{
           public static void main(String args[]){
                     System.out.println("Hello Nayyar!");
                     System.out.println("Hello NayyarKhan!!");
+                    System.out.println("Hello NayyarKhan!!!");
+
 
           }
 }
